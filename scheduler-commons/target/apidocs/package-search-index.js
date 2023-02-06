c709f630691b8b7ee6cb9edc615d7050c21c09ce
Cirillo Luca [vbq53075] <luca.cirillo@igt.com>
2023-02-06 13:33:52 +0100

    re

--- scheduler-commons/target/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.igt.eInstants.scheduler"},{"l":"com.igt.eInstants.scheduler.conf"},{"l":"com.igt.eInstants.scheduler.job"}]
